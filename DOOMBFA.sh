a1ed34cbc0ee7ae33ae67e5ff0301f9f51392484
MadDeCoDeR <18539489+MadDeCoDeR@users.noreply.github.com>
2025-10-28 11:32:08 +0200

    Update things around for upcoming update (NOTE: Some versions and urls are not final)

--- DOOMBFA.sh
+++ DOOMBFA.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-if [[ " $@ " =~ " -open-launcher " ]]
-then
-   mkdir -p "$XDG_DATA_HOME/.doombfa/base/lib"
-   export LD_LIBRARY_PATH="$XDG_DATA_HOME/.doombfa/base/lib"
-   (cd "$XDG_DATA_HOME/.doombfa" && mono /app/bin/CDL.exe $*)
-else
-   mkdir -p "$XDG_DATA_HOME/.doombfa/base/lib"
-   export LD_LIBRARY_PATH="$XDG_DATA_HOME/.doombfa/base/lib"
-   (cd "$XDG_DATA_HOME/.doombfa/base/lib" && exec /app/bin/DoomBFA $*)
-fi
+
+mkdir -p "$XDG_DATA_HOME/.doombfa/base/lib"
+export LD_LIBRARY_PATH="$XDG_DATA_HOME/.doombfa/base/lib"
+(cd "$XDG_DATA_HOME/.doombfa/base/lib" && exec /app/bin/DoomBFA $*)
+
