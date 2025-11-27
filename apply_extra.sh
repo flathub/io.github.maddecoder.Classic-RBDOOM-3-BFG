3deff2cb6bc3f2e05b3fbd3c9e1e570cd5e7e959
MadDeCoDeR <18539489+MadDeCoDeR@users.noreply.github.com>
2025-11-27 20:50:48 +0200

    Version update to 1.4.1

--- apply_extra.sh
+++ apply_extra.sh
@@ -5,8 +5,8 @@ cp -R /app/share/games/doombfa/base/* games/doombfa/base
 cp -R /app/share/games/doombfa/base_BFG/* games/doombfa/base_BFG
 cp -R /app/share/games/doombfa/* games/doombfa
 unzip BFA-Assets.zip
-cp -R BFA-Assets-1.4.0/base/* games/doombfa/base
-cp -R BFA-Assets-1.4.0/base_BFG/* games/doombfa/base_BFG
-cp -R BFA-Assets-1.4.0/* games/doombfa
-rm -r BFA-Assets-1.4.0
+cp -R BFA-Assets-1.4.1/base/* games/doombfa/base
+cp -R BFA-Assets-1.4.1/base_BFG/* games/doombfa/base_BFG
+cp -R BFA-Assets-1.4.1/* games/doombfa
+rm -r BFA-Assets-1.4.1
 rm BFA-Assets.zip
